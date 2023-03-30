c410f4acf4abb9d412598b13700c59a3b102b3b4
Rangga857 <azharrangga28@gmail.com>
2023-03-30 15:14:42 +0700

    langkah Algoritm NO 1

--- QuickSort_150/QuickSort_150.cpp
+++ QuickSort_150/QuickSort_150.cpp
@@ -39,3 +39,11 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)						// langkah Algoritm NO 1
+		return;
+		
+}
