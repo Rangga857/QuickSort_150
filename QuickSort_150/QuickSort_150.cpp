56016bc704fc2a9a63870810ca16a3e5eeb67937
Rangga857 <azharrangga28@gmail.com>
2023-03-30 15:00:42 +0700

    array of integers to hold values, Number of comparasion, Number of data movements

---
+++ QuickSort_150/QuickSort_150.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];			// Array of integers to hold values
+int camp_count = 0;		// Number of comparasion
+int mov_count = 0;		// Number of data movements
